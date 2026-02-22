5bc977ef732d5ea69616823ebfaa56b71b6e473b
pragmat1c <chris@chrisjweber.com>
2026-02-22 09:50:59 -0600

    elanmoc2: Add support for ELAN 04f3:0ca2 (LG laptop)
    Device sends 70-byte responses for cmd_finger_info rather than 64.
    Tested and working on Pop!_OS 24.04 with LG gram 17 2025 laptop.
    Model 17Z90TL-H.AUB6U3

--- libfprint/drivers/elanmoc2/elanmoc2.h
+++ libfprint/drivers/elanmoc2/elanmoc2.h
@@ -113,7 +113,7 @@ static const struct elanmoc2_cmd cmd_get_fw_ver = {
 static const struct elanmoc2_cmd cmd_finger_info = {
   .cmd = {0xff, 0x12},
   .out_len = 4,
-  .in_len = 64,
+  .in_len = 70,
   .ep_in = ELANMOC2_EP_CMD_IN,
 };
 
@@ -211,5 +211,6 @@ static const FpIdEntry elanmoc2_id_table[] = {
   {.vid = ELANMOC2_VEND_ID, .pid = 0x0c00, .driver_data = ELANMOC2_ALL_DEV},
   {.vid = ELANMOC2_VEND_ID, .pid = 0x0c4c, .driver_data = ELANMOC2_ALL_DEV},
   {.vid = ELANMOC2_VEND_ID, .pid = 0x0c5e, .driver_data = ELANMOC2_DEV_0C5E},
+  {.vid = ELANMOC2_VEND_ID, .pid = 0x0ca2, .driver_data = ELANMOC2_ALL_DEV},
   {.vid = 0, .pid = 0, .driver_data = ELANMOC2_DEV_0C4C}
 };
